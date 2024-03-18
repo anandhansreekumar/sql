758318e51f1e8715d2555e63335f0ac40c259ed6
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-03-18 12:44:21 +0530

    feat: sub query

---
+++ 028-sub-squery.sql
@@ -0,0 +1,25 @@
+-- SELECT
+--     customer_name,
+--     product_name
+-- FROM
+--     (
+--         SELECT
+--             *
+--         FROM
+--             sales
+--         WHERE
+--             volume > 1000
+--     )
+-- CREATE VIEW base_result AS
+-- SELECT
+--     *
+-- FROM
+--     sales
+-- WHERE
+--     volume > 1000;
+
+SELECT
+    customer_name,
+    product_name
+FROM
+    base_result
