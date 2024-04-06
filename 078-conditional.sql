0b8a737492f342a53a2322d2b4738950f24113a8
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-04-06 12:47:25 +0530

    feat: conditional expressions

--- 078-conditional.sql
+++ 078-conditional.sql
@@ -1,10 +1,32 @@
+-- SELECT
+--     amount_billed,
+--     CASE
+--         WHEN amount_billed > 15
+--         AND amount_billed <  THEN 'Normal day'
+--         WHEN amount_billed > 30 THEN 'Good day'
+--         ELSE 'Bad day'
+--     END
+-- FROM
+--     orders;
 SELECT
-    amount_billed,
+    day_number,
     CASE
-        WHEN amount_billed > 15
-        AND amount_billed < 30 THEN 'Normal day'
-        WHEN amount_billed > 30 THEN 'Good day'
-        ELSE 'Bad day'
+        WHEN day_number = 0 THEN 'Monday'
+        WHEN day_number = 1 THEN 'Tuesday'
+        WHEN day_number = 2 THEN 'Wednesday'
+        WHEN day_number = 3 THEN 'Thursday'
+        WHEN day_number = 4 THEN 'Friday'
+        WHEN day_number = 5 THEN 'Saturday'
+        WHEN day_number = 6 THEN 'Sunday'
     END
 FROM
-    orders;
+    (
+        SELECT
+            EXTRACT(
+                ISODOW
+                FROM
+                    last_checkin
+            ) as day_number
+        FROM
+            memberships
+    ) as weekday_numbers;
