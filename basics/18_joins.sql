ce08343aadee59fa982b8381bed86b8ce8353ece
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-07-04 14:42:53 +0530

    feat: order of operations

--- basics/18_joins.sql
+++ basics/18_joins.sql
@@ -4,13 +4,14 @@
 -- FROM employees as e
 --     INNER JOIN salaries as s ON s.emp_no = e.emp_no
 -- ORDER BY e.emp_no
-SELECT e.emp_no,
-    CONCAT(e.first_name, ' ', e.last_name) as "name",
-    t.title,
-    s.salary,
-    t.from_date as "promoted_on"
-FROM employees as e
-    INNER JOIN salaries as s ON s.emp_no = e.emp_no
-    INNER JOIN titles as t on t.emp_no = e.emp_no
-    AND t.from_date = s.from_date + INTERVAL '2 days'
-ORDER BY e.emp_no
+-- SELECT e.emp_no,
+--     CONCAT(e.first_name, ' ', e.last_name) as "name",
+--     t.title,
+--     s.salary,
+--     t.from_date as "promoted_on"
+-- FROM employees as e
+--     INNER JOIN salaries as s ON s.emp_no = e.emp_no
+--     INNER JOIN titles as t on t.emp_no = e.emp_no
+--     AND t.from_date = s.from_date + INTERVAL '2 days'
+-- ORDER BY e.emp_no
+
--- readme.md
+++ readme.md
@@ -12,3 +12,13 @@ AVG,COUNT,MIN,MAX,SUM,...
 
 '' => for strings
 "" => for table/column names
+
+### Order of operations
+
+FROM clause (including any JOIN operations)
+WHERE clause
+GROUP BY clause (if present)
+HAVING clause (if present)
+SELECT clause
+ORDER BY clause
+LIMIT clause (if present)
