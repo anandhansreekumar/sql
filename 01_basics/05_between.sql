dd86f9b398e0998a2469607188f55051018a9454
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-07-08 14:35:30 +0530

    feat: between and not between

--- 01_basics/05_between.sql
+++ 01_basics/05_between.sql
@@ -8,3 +8,15 @@ WHERE age BETWEEN 30 AND 50
 SELECT AVG(income)
 FROM customers
 WHERE age BETWEEN 20 AND 50;
+-- SELECT 
+--   payment_id, 
+--   amount, 
+--   payment_date 
+-- FROM 
+--   payment 
+-- WHERE 
+--   payment_date BETWEEN '2007-02-15' AND '2007-02-20' 
+--   AND amount > 10 
+-- ORDER BY 
+--   payment_date;
+-- NOT BETWEEN
