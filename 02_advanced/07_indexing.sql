7c6a922647e75d7aa90f1e2f225e00a549daa58d
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-07-16 11:25:56 +0530

    feat: indexing

--- 02_advanced/07_indexing.sql
+++ 02_advanced/07_indexing.sql
@@ -1,2 +1,6 @@
 CREATE INDEX idx_title ON titles(title);
 DROP INDEX idx_title;
+-- CREATE UNIQUE INDEX 
+-- Partial index 
+CREATE INDEX idx_title ON titles(title)
+WHERE title IN ('Senior Engineer', 'Assistant Engineer')
